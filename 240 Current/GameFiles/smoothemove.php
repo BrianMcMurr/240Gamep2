f5c34a10744719ef79a511e1fbebce14096035d7
Andrewab <andrewalcideblanchette@gmail.com>
2020-05-12 16:26:48 -0700

    small changes to file paths

--- 240 Current/GameFiles/smoothemove.php
+++ 240 Current/GameFiles/smoothemove.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<!--setInterval(stop(), 60000));
-
--->
 <html>
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
--- 240 Current/Login/Login.php
+++ 240 Current/Login/Login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <audio id="myAudio">
-  <source src="../PreGame/MusicPic/MenuSong.mp3" type="audio/mp3">
+  <source src="../MusicPic/MenuSong.mp3" type="audio/mp3">
   Your browser does not support the audio element.
 </audio>
 
